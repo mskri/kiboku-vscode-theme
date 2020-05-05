8793206f1e29714d5edc135f31cad471cf13c4de
Sakari Mursu <sakari@mursu.dev>
2020-05-05 22:45:45 +0300

    Add code to generate theme
    Inspired by https://github.com/juliettepretot/lucy-vscode-theme

---
+++ build.js
@@ -0,0 +1,35 @@
+const { writeFile } = require("fs");
+const { promisify } = require("util");
+const getTheme = require("./theme.js");
+const colors = require("./colors.js");
+
+const themeName = 'kiboku';
+const promisifiedWriteFile = promisify(writeFile);
+
+const buildTheme = async () => {
+  try {
+    const themeWithColors = getTheme({
+      name: themeName,
+      colors: Object.entries(colors).reduce(
+        (colorScheme, [colorName, colorValue]) => ({
+          ...colorScheme,
+          [colorName]: colorValue,
+        }),
+        {}
+      ),
+    });
+
+
+
+    await promisifiedWriteFile(
+      `./dist/${themeName}-color-theme.json`,
+      JSON.stringify(themeWithColors)
+    )
+
+    console.log("Theme built")
+  } catch (error) {
+    console.error('Error building theme:', error);
+  }
+};
+
+buildTheme();
