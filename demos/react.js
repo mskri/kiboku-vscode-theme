f98079ebbd44f039bd7dd5526174c188ba408477
Sakari Mursu <sakari@mursu.dev>
2022-03-29 09:05:25 +0300

    Update react.js demo

--- demos/react.js
+++ demos/react.js
@@ -2,13 +2,25 @@ import React, { useContext } from 'react';
 import Todo from './todo';
 import { TodoAppContext } from './index';
 
+// Something and something else
 export default function Todos(props) {
 	const { todoList, handleRemoveItem } = props;
 	const theme = useContext(TodoAppContext);
 	const numberOfTodos = 3;
 	const isVisible = false;
+	const back = null;
 
-	console.log(numberOfTodos);
+	const defaultValues = {
+		theme,
+		back,
+		value: 1234,
+	};
+
+	console.log('Hello!', numberOfTodos, back);
+
+	if (back) {
+		return defaultValues;
+	}
 
 	return (
 		<ul className={theme.mode}>
